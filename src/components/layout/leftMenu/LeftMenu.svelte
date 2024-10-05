1288088bfc8f7e51c42fac76be91a4153a76f0ce
Altho <alan.at.thomas@gmail.com>
2024-10-05 06:58:35 -0500

    added styling

---
+++ src/components/layout/leftMenu/LeftMenu.svelte
@@ -0,0 +1,18 @@
+<nav class="list-nav">
+    <ul>
+        <li><a href="Character">Character</a></li>
+        <li><a href="Inventory">Inventory</a></li>
+        <li><a href="Stronghold">Stronghold</a></li>
+        <hr/>
+        <li><a href="Attributes">Attributes</a></li>
+        <li><a href="Skills">Skills</a></li>
+        <li><a href="Weapons">Weapons</a></li>
+        <hr/>
+        <li><a href="Reference">Reference</a></li>
+        <li><a href="Lookup table">Lookup table</a></li>
+    </ul>
+</nav>
+
+<style>
+
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>import "../app.css";
+import '../global.css'
+import {AppShell} from "@skeletonlabs/skeleton";
+import LeftMenu from "../components/layout/leftMenu/LeftMenu.svelte";</script>
+
+
+
+
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+    <svelte:fragment slot="header"></svelte:fragment>
+    <svelte:fragment slot="sidebarLeft">
+        <LeftMenu />
+    </svelte:fragment>
+    <!-- (sidebarRight) -->
+    <!-- (pageHeader) -->
+    <!-- Router Slot -->
+    <slot />
+    <!-- ---- / ---- -->
+    <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+    <!-- (footer) -->
+</AppShell>
