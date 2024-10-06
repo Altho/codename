f4c0bbe17005df12de5d299758d371451ffc63ed
Altho <alan.at.thomas@gmail.com>
2024-10-05 22:31:16 -0500

    added attributes types

--- src/types/TGameplay.ts
+++ src/types/TGameplay.ts
@@ -5,16 +5,29 @@ export type Dice = {
     triggerEffect: number;
 }
 
-
-
 export interface CastResult {
     value: number;
     didTriggerEffect: boolean;
     effectAmount: number;
 }
 
-export type SkillEffect = {
-    isTwoTimes: boolean;
-    bonusAmount: number;
-    affectedCategory: string; //TODO: Add an enum
+export type Attribute = {
+    value: number;
+    skills: Skill[]
 }
+
+export type Skill = {
+    value: number;
+    bonus: number;
+    groupBonus: number;
+    levelBonus: number;
+    other: number;
+    total: number;
+    DcEffect: string;
+}
+
+// export type SkillEffect = {
+//     isTwoTimes: boolean;
+//     bonusAmount: number;
+//     affectedCategory: string; //TODO: Add an enum
+// }
