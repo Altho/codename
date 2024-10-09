917c6df524772460461f5e94faa46abdb78b75be
Altho <alan.at.thomas@gmail.com>
2024-10-08 22:58:03 -0500

    added container

--- src/components/skills/Attribute.svelte
+++ src/components/skills/Attribute.svelte
@@ -7,9 +7,11 @@
         skill.value++
     }
 </script>
-
-<div>
-    {skill.name}
-    <div>{skill.value} | {skill.bonus()} | {skill.levelBonus()} | {skill.groupBonus()} | {skill.total} | {skill.DcEffect}</div>
+<div class="container mx-auto p-8 space-y-8">
+    <div>
+        {skill.name}
+        <div>{skill.value} | {skill.bonus()} | {skill.levelBonus()} | {skill.groupBonus()} | {skill.total} | {skill.DcEffect}</div>
+    </div>
+    <div> <button class="border-2 m-2 p-2" on:click={handleClick}>Add +1 to base value</button> <button class="border-2 m-2 p-2" on:click={() => skill.value = 0}>Reset</button></div>
 </div>
-<div> <button class="border-2 m-2 p-2" on:click={handleClick}>Add +1 to base value</button> <button class="border-2 m-2 p-2" on:click={() => skill.value = 0}>Reset</button></div>
+
