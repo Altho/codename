7374ef84e409f93aa4f6da8d1626ce479b25963e
Altho <alan.at.thomas@gmail.com>
2024-10-08 22:09:28 -0500

    separated group and level bonus functions

--- src/helpers/SkillFunctions.ts
+++ src/helpers/SkillFunctions.ts
@@ -3,7 +3,12 @@ export const bonus = (p: number):number => {
     return Math.floor( 0.5 * (Math.sqrt( 8 * p) +1))
 }
 
-export const ceilBonus = (p: number):number => {
+export const levelBonus = (p: number):number => {
     return Math.ceil(bonus(p))
 }
+
+export const GroupBonus = (p: number):number => {
+    return Math.ceil(bonus(p))
+}
+
 
