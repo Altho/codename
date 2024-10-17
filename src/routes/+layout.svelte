08a11bf1fff9d730d8873cf149557488b65deddc
Altho <alan.at.thomas@gmail.com>
2024-10-16 21:54:45 -0500

    profiles checl

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,78 @@
-<script lang="ts">import "../app.css";
-import '../global.css'
-import {AppShell} from "@skeletonlabs/skeleton";
-import LeftMenu from "$lib/components/layout/leftMenu/LeftMenu.svelte";
-import { onMount } from 'svelte'
-import { supabase } from '$lib/db/client'
-import type { AuthSession } from '@supabase/supabase-js'
-import Account from '$lib/components/auth/Account.svelte'
-import Auth from '$lib/components/auth/Auth.svelte'
-import { initializeStores, Toast } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import '../app.css';
+	import '../global.css';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import LeftMenu from '$lib/components/layout/leftMenu/LeftMenu.svelte';
+	import { onMount } from 'svelte';
+	import { characters } from '$lib/stores/characters';
+	import { supabase } from '$lib/db/client';
+	import type { AuthSession } from '@supabase/supabase-js';
+	import Account from '$lib/components/auth/Account.svelte';
+	import Auth from '$lib/components/auth/Auth.svelte';
+	import { initializeStores, Toast } from '@skeletonlabs/skeleton';
 
-initializeStores();
+	initializeStores();
 
-let session: AuthSession | null
+	let session: AuthSession | null;
 
-onMount(() => {
-    supabase.auth.getSession().then(({ data }) => {
-        session = data.session
-    })
+	onMount(async () => {
+		const { data: sessionData } = await supabase.auth.getSession();
+		session = sessionData.session;
 
-    supabase.auth.onAuthStateChange((_event, _session) => {
-        session = _session
-    })
-})
+		supabase.auth.onAuthStateChange((_event, _session) => {
+			session = _session;
+		});
 
+		if (session?.user.id) {
+			const { data, error } = await supabase
+				.from('characters')
+				.select(
+					`
+                *,
+                profiles!inner(*)
+            `
+				)
+				.eq('profiles.id', session.user.id);
 
+			if (!error) {
+				console.log(data);
+				$characters.push(data);
+			} else {
+				console.error('Error:', error);
+			}
+		} else {
+			console.log('No session available');
+		}
+		console.log($characters.length);
+	});
 </script>
 
 <Toast />
 
 {#if !session}
-    <div class="flex justify-center items-center h-full">
-    <Auth />
-    </div>
+	<div class="flex justify-center items-center h-full">
+		<Auth />
+	</div>
+{/if}
+
+{#if $characters.length === 0}
+	<div class="flex justify-center items-center h-full">
+		You don't have any character --- Create character Create DM Profile ---
+	</div>
 {:else}
-<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
-    <svelte:fragment slot="header"></svelte:fragment>
-    <svelte:fragment slot="sidebarLeft">
-        <LeftMenu />
-    </svelte:fragment>
-    <!-- (sidebarRight) -->
-    <!-- (pageHeader) -->
-    <!-- Router Slot -->
+	<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+		<svelte:fragment slot="header"></svelte:fragment>
+		<svelte:fragment slot="sidebarLeft">
+			<LeftMenu />
+		</svelte:fragment>
+		<!-- (sidebarRight) -->
+		<!-- (pageHeader) -->
+		<!-- Router Slot -->
 
+		<slot />
 
-
-        <slot/>
-
-
-
-    <!-- ---- / ---- -->
-    <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-    <!-- (footer) -->
-</AppShell>
+		<!-- ---- / ---- -->
+		<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+		<!-- (footer) -->
+	</AppShell>
 {/if}
